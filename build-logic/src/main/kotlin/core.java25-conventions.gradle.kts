9d3b391737c9f3f3faaa53e6fcf91fc74fd9f8c3
BestBearr <32557819+bestbearr@users.noreply.github.com>
2026-07-31 00:34:03 -0400

    chore: add Java 21 and Java 25 convention Gradle scripts

---
+++ build-logic/src/main/kotlin/core.java25-conventions.gradle.kts
@@ -0,0 +1,25 @@
+/*
+ * This file is part of AlpineCore - https://github.com/alpine-network/alpine-core
+ * Copyright (C) 2025 Crystal Development, LLC
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+plugins {
+    id("core.base-conventions")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_25
+    targetCompatibility = JavaVersion.VERSION_25
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.release.set(25)
+}
+
+tasks.withType<Javadoc>().configureEach {
+    (options as StandardJavadocDocletOptions).source = "25"
+}
